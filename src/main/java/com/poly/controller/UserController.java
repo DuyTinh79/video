9d353deba2c3455633cf4377d4a92abf5a0a4c9a
DuyTinh79 <duytinh792002@gmail.com>
2023-07-31 12:44:56 +0700

    init project

---
+++ src/main/java/com/poly/controller/UserController.java
@@ -0,0 +1,93 @@
+package com.poly.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.poly.constant.SessionAttr;
+import com.poly.entity.User;
+import com.poly.entity.Video;
+import com.poly.service.UserService;
+import com.poly.service.VideoService;
+import com.poly.service.impl.UserServiceImpl;
+import com.poly.service.impl.VideoServiceImpl;
+
+@WebServlet(urlPatterns = { "/login", "/logout", "/register" })
+public class UserController extends HttpServlet {
+
+	private static final long serialVersionUID = -5860351843059541642L;
+	
+	private UserService userService = new UserServiceImpl();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String path = req.getServletPath();
+		switch (path) {
+		case "/login":
+			doGetLogin(req, resp);
+			break;
+		case "/register":
+			doGetRegister(req, resp);
+			break;
+		}
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session	= req.getSession();
+		String path = req.getServletPath();
+		switch (path) {
+		case "/login":
+			doPostLogin(session, req, resp);
+			break;
+		case "/register":
+			doPostRegister(session, req, resp);
+			break;
+		}
+	}
+
+	private void doGetLogin(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/views/user/login.jsp");
+		requestDispatcher.forward(req, resp);
+	}
+
+	private void doGetRegister (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/views/user/register.jsp");
+		requestDispatcher.forward(req, resp);
+	}
+	
+	private void doPostLogin(HttpSession session, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		User user = userService.login(username, password);	
+		
+		if (user != null) {
+			session.setAttribute(SessionAttr.CURRENT_USER, user);
+			resp.sendRedirect("index");
+		}else {
+			resp.sendRedirect("login");
+		}
+	}
+	
+	private void doPostRegister(HttpSession session, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		String email = req.getParameter("email");
+		
+		User user = userService.register(username, password, email);
+		
+		if (user != null) {
+			session.setAttribute(SessionAttr.CURRENT_USER, user);
+			resp.sendRedirect("index");
+		}else {
+			resp.sendRedirect("register");
+		}
+	}
+}
